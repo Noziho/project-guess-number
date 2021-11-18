4097045df512e3a154a810d5c0ae85c1c477a79f
Noah2211 <noah.decroix3@gmail.com>
2021-11-18 20:21:28 +0100

    Almost done project + changes

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
 let solution = Math.trunc(Math.random() * 100 );
 console.log(solution);
-let lives = 10;
+let lifes = 9;
+let textLoose = document.getElementById("loose");
 let divItem = document.getElementById("items");
 let inputNumber = document.getElementById("foundNumber");
 let button = document.getElementById("valide-button");
 let divForNumber = document.createElement('div');
+let create = document.createElement('p');
 
+
+
+if (lifes > - 1) {
     button.addEventListener("click", function (){
         divForNumber.innerHTML = inputNumber.value;
         divForNumber.style.fontSize = "15rem";
         divItem.append(divForNumber);
+        lifes--;
+        console.log(lifes)
+
         if (parseInt(inputNumber.value) === solution){
-            let create = document.createElement('p');
+
             create.innerHTML = "Congrats it's a great number"
             create.style.color = "green";
             divItem.append(create);
             divForNumber.style.color = "green";
         }
+        else if (parseInt(inputNumber.value) < solution) {
+            textLoose.innerHTML = "Le chiffre à trouver est plus grand";
+        }
 
-        else {
+        else if (parseInt(inputNumber.value) > solution) {
+            textLoose.innerHTML = "Le chiffre à trouver est plus petit";
+        }
+
+       if (parseInt(inputNumber.value) !== solution) {
             divForNumber.style.color = "red";
+            create.remove();
+            console.log(true);
+
         }
 
     })
+} else {
+    textLoose.innerHTML = "You have loose";
+    console.log(true);
+}
+
+
+
+
+
 
 
