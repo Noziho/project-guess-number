31cb5619fadb4b7cea563ed3de728eec1ad284f2
Noah2211 <noah.decroix3@gmail.com>
2021-11-19 09:56:08 +0100

    Done project

--- app.js
+++ app.js
@@ -7,44 +7,57 @@ let inputNumber = document.getElementById("foundNumber");
 let button = document.getElementById("valide-button");
 let divForNumber = document.createElement('div');
 let create = document.createElement('p');
+let reset = document.getElementById("reset");
+let winReset = document.getElementById("win");
 
 
 
-if (lifes > - 1) {
-    button.addEventListener("click", function (){
+
+button.addEventListener("click", function (){
+
+    if (lifes === 0) {
+
+        textLoose.innerHTML = "You have loose !";
+        textLoose.style.color = "red";
+        textLoose.style.fontSize = "2rem";
+        reset.innerHTML = "Try Again !";
+        reset.style.color = "red";
+        reset.style.fontSize = "2rem";
+        button.remove();
+
+    }
+    else if (parseInt(inputNumber.value) === solution){
         divForNumber.innerHTML = inputNumber.value;
         divForNumber.style.fontSize = "15rem";
         divItem.append(divForNumber);
-        lifes--;
-        console.log(lifes)
+        create.innerHTML = "Congrats it's a great number"
+        create.style.color = "green";
+        divItem.append(create);
+        divForNumber.style.color = "green";
+        winReset.style.display = "block";
 
-        if (parseInt(inputNumber.value) === solution){
+    }
+    else if (parseInt(inputNumber.value) < solution) {
+        textLoose.innerHTML = "Le chiffre à trouver est plus grand";
+    }
 
-            create.innerHTML = "Congrats it's a great number"
-            create.style.color = "green";
-            divItem.append(create);
-            divForNumber.style.color = "green";
-        }
-        else if (parseInt(inputNumber.value) < solution) {
-            textLoose.innerHTML = "Le chiffre à trouver est plus grand";
-        }
+    else if (parseInt(inputNumber.value) > solution) {
+        textLoose.innerHTML = "Le chiffre à trouver est plus petit";
+    }
 
-        else if (parseInt(inputNumber.value) > solution) {
-            textLoose.innerHTML = "Le chiffre à trouver est plus petit";
-        }
+   if (parseInt(inputNumber.value) !== solution) {
+       divForNumber.innerHTML = inputNumber.value;
+       divForNumber.style.fontSize = "15rem";
+       divItem.append(divForNumber);
+       lifes--;
+       console.log(lifes)
+       divForNumber.style.color = "red";
+       create.remove();
+       console.log(true);
 
-       if (parseInt(inputNumber.value) !== solution) {
-            divForNumber.style.color = "red";
-            create.remove();
-            console.log(true);
+    }
 
-        }
-
-    })
-} else {
-    textLoose.innerHTML = "You have loose";
-    console.log(true);
-}
+})
 
 
 
